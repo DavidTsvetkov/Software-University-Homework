6f3eab877cc46a65c0955ffd381d7f5450bc2414
DavidTsvetkov <44206509+DavidTsvetkov@users.noreply.github.com>
2018-12-02 13:33:16 +0200

    Basic Syntax - First Task
    Technology Fundamentals 4.0

---
+++ 01.StudentInformation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.StudentInformation
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+    }
+}
